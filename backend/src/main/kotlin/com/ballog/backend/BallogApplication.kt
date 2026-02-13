8172239e07870ecc717cc9841cceec73606e5d24
kdohyeon <kdohyeon@gmail.com>
2026-02-13 15:07:09 +0900

    feat(backend): Enable async processing

--- backend/src/main/kotlin/com/ballog/backend/BallogApplication.kt
+++ backend/src/main/kotlin/com/ballog/backend/BallogApplication.kt
@@ -3,6 +3,9 @@ package com.ballog.backend
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+import org.springframework.scheduling.annotation.EnableAsync
+
+@EnableAsync
 @SpringBootApplication
 class BallogApplication
 
